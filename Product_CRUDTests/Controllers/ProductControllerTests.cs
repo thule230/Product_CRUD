b6e9447441799695c0af4e9303d8d2b77054ef7e
Arthur Oliveira <thule.soares@gmail.com>
2023-03-24 00:24:18 +0000

    Restructured the folders to include the tests in Git

---
+++ Product_CRUDTests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Product_CRUD.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product_CRUD.Controllers.Tests
+{
+    [TestClass()]
+    public class ProductControllerTests
+    {
+
+    }
+}
